ea2283486d369b7e870ac79655e0c528a2d5425e
Murat Yaşar <127419303+murat-yasar@users.noreply.github.com>
2023-05-16 09:22:29 +0200

    Update main.js
    Alternative methods added

--- js_counter/main.js
+++ js_counter/main.js
@@ -5,17 +5,47 @@ let btnDec = document.querySelector('#btn-dec');
 let btnRes = document.querySelector('#btn-reset');
 let count = 0;
 
-btnInc.addEventListener('click', function(){
-  count++;
-  counter.innerHTML = count;
-});
+//* Alt-1
+// btnInc.addEventListener('click', incNum);
+// function incNum(){
+//   counter.innerHTML = ++count;
+// }
 
-btnDec.addEventListener('click', function(){
-  count--;
-  counter.innerHTML = count;
-});
+// btnDec.addEventListener('click', decNum);
+// function decNum(){
+//   counter.innerHTML = --count;
+// }
 
-btnRes.addEventListener('click', function(){
+// btnRes.addEventListener('click', resetNum);
+// function resetNum(){
+//   count = 0;
+//   counter.innerHTML = count;
+// }
+
+
+//* Alt-2
+// btnInc.addEventListener('click', function(){
+//   count++;
+//   counter.innerHTML = count;
+// });
+
+// btnDec.addEventListener('click', function(){
+//   count--;
+//   counter.innerHTML = count;
+// });
+
+// btnRes.addEventListener('click', function(){
+//   count = 0;
+//   counter.innerHTML = count;
+// });
+
+
+//* Alt-3
+btnInc.addEventListener('click', ()=>{counter.innerHTML = ++count;});
+
+btnDec.addEventListener('click', ()=>{counter.innerHTML = --count;});
+
+btnRes.addEventListener('click', ()=>{
   count = 0;
   counter.innerHTML = count;
 });
